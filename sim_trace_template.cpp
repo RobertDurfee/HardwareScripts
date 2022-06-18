d3e47f9179b49133fe522e2544321b7dedc7f839
Robert Durfee <robertdurfee21@icloud.com>
2022-06-18 14:36:07 -0500

    post-synthesis simulation

--- sim_trace_template.cpp
+++ sim_trace_template.cpp
@@ -1,5 +1,6 @@
 #include "V$MODULE.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 int main(int argc, char **argv, char **env) {
   VerilatedContext *context = new VerilatedContext;
@@ -7,12 +8,21 @@ int main(int argc, char **argv, char **env) {
 
   V$MODULE *top = new V$MODULE { context };
 
+  context->traceEverOn(true);
+  VerilatedVcdC *trace = new VerilatedVcdC;
+  top->trace(trace, $DEPTH);
+  trace->open("$TRACE");
+
   while ($STOP) {
     context->timeInc(1);
     top->$CLOCK = ~top->$CLOCK;
     top->eval();
+    trace->dump(context->time());
   }
 
+  trace->close();
+
+  delete trace;
   delete top;
   delete context;
 
